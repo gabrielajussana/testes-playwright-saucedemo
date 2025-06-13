900531b9c7123c0dc76ade23377b9abe1f18551b
Gabriela Jussana <gabrielajussana@gmail.com>
2025-06-13 11:10:12 -0300

    feat(cart): setup cart page model

---
+++ src/pages/cart.page.ts
@@ -0,0 +1,36 @@
+import { Page, Locator } from '@playwright/test';
+
+export class CartPage {
+  readonly page: Page;
+  readonly checkoutButton: Locator;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly postalCodeInput: Locator;
+  readonly continueButton: Locator;
+  readonly finishButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.checkoutButton = page.locator('#checkout');
+    this.firstNameInput = page.locator('#first-name');
+    this.lastNameInput = page.locator('#last-name');
+    this.postalCodeInput = page.locator('#postal-code');
+    this.continueButton = page.locator('#continue');
+    this.finishButton = page.locator('#finish');
+  }
+
+  async checkout() {
+    await this.checkoutButton.click();
+  }
+
+  async fillShippingInfo(firstName: string, lastName: string, postalCode: string) {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.postalCodeInput.fill(postalCode);
+    await this.continueButton.click();
+  }
+
+  async finishPurchase() {
+    await this.finishButton.click();
+  }
+}
